c8843027339b701bb87e50ec7d910e33769ca773
Carryours <Carryours@aliyun.com>
2024-11-18 10:02:08 +0800

    add priview content

--- src/views/Tinymce.vue
+++ src/views/Tinymce.vue
@@ -1,7 +1,7 @@
 <script setup>
 import Editor from '@tinymce/tinymce-vue'
 import axios from 'axios'
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 onMounted(() => {
 
 })
@@ -10,7 +10,7 @@ let content = null;
 let imgList = [];
 
 let editor = null;
-
+let isView = ref(false);
 const handlePaste = async function (plugin, args) {
   imgList = args.content || args.node ? getImagesFromNode(args.node) : [];
 }
@@ -51,7 +51,9 @@ const handleClick = async function () {
       currentContent = currentContent.replace(imgList[index], item);
     })
     editor.setContent(currentContent);
-    // console.log(currentContent);
+    console.log(currentContent);
+
+    content = currentContent
   })
 
 }
@@ -59,19 +61,30 @@ const onInit = function (event) {
   editor = event.target;
   console.log(editor)
 }
+const testView = function () {
+  isView.value = !isView.value;
+}
 </script>
 <template>
-  <div style="width: 80vw;">
-    <Editor v-model="content" api-key="no-api-key" tinymceScriptSrc="/tinymce/tinymce.min.js" @init="onInit" :init="{
-      branding: false,
-      plugins: 'lists link image table code help wordcount',
-      paste_data_images: true,
-      paste_postprocess: handlePaste
-    }" />
-    <button @click="handleClick">testtt</button>
-    <div>
-      <!-- <img style="width: 400px; height: 100%" id="testImg"
-        src="https://p3-sign.toutiaoimg.com/tos-cn-i-tjoges91tu/aa2d73aaa8c5addc555dc33866b380bd~noop.image?_iz=58558&from=article.pc_detail&lk3s=953192f4&x-expires=1731902845&x-signature=gQCTJeWIhceA8bWZp0il%2BBMm0gk%3D" /> -->
+  <div>
+    <div style="width: 50vw;" v-if="!isView">
+      <Editor v-model="content" api-key="no-api-key" tinymceScriptSrc="/tinymce/tinymce.min.js" @init="onInit" :init="{
+        branding: false,
+        plugins: 'lists link image table code help wordcount',
+        paste_data_images: true,
+        paste_postprocess: handlePaste
+      }" />
+      <button @click="handleClick">Test</button>
+      <button @click="testView">预览</button>
+      <div>
+        <!-- <img style="width: 400px; height: 100%" id="testImg"
+          src="https://p3-sign.toutiaoimg.com/tos-cn-i-tjoges91tu/aa2d73aaa8c5addc555dc33866b380bd~noop.image?_iz=58558&from=article.pc_detail&lk3s=953192f4&x-expires=1731902845&x-signature=gQCTJeWIhceA8bWZp0il%2BBMm0gk%3D" /> -->
+      </div>
+    </div>
+  
+    <div v-if="isView" style="height: 100%">
+      <div v-html="content"></div>
+      <button @click="testView">返回</button>
     </div>
   </div>
 
